b5a897c8e660035dbba338d039d1136de8d2638d
Adam Maxwell <catalyst256@gmail.com>
2016-06-02 18:01:47 +0100

    push with more config

---
+++ libs/colours.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+class Colours(object):
+    N = '\033[m' # native
+    R = '\033[31m' # red
+    G = '\033[32m' # green
+    O = '\033[33m' # orange
+    B = '\033[34m' # blue
--- rubberband.py
+++ rubberband.py
@@ -3,22 +3,35 @@
 import cmd
 import elasticsearch
 import json
+from libs.colours import Colours
+import ConfigParser
+
+index = '*'
 
 
 class RubberBand(cmd.Cmd):
-    prompt = 'rubberband: '
-    intro = "ElasticSearch Simple Search Engine"
+    prompt = Colours.B + 'rubberband: ' + Colours.N
+    intro = Colours.O + "ElasticSearch Simple Search Engine" + Colours.N
 
     def do_search(self, line):
-        es = elasticsearch.Elasticsearch('192.168.2.143')
-        results = es.search(index='test', q='{0}'.format(line))
-        data = results['hits']
-        if data['total'] > 0:
-            for d in data['hits']:
-                print json.dumps(d['_source'], sort_keys=True, indent=4)
-        else:
-            print 'No results found'
+        try:
+            conf = ConfigParser.ConfigParser()
+            conf.read("rubberband.conf")
+            es = elasticsearch.Elasticsearch(conf.get('elastic', 'server'))
+            results = es.search(index=index, q='{0}'.format(line))
+            data = results['hits']
+            if data['total'] > 0:
+                for d in data['hits']:
+                    print json.dumps(d['_source'], sort_keys=True, indent=4)
+            else:
+                print Colours.R + 'No results found' + Colours.N
+        except Exception as e:
+            print Colours.R + '[!] Search Error: {0}'.format(str(e)) + Colours.N
 
+    def do_index(self, eindex):
+        global index
+        index = eindex
+        print Colours.G + '[-] Index set to: {0}'.format(index) + Colours.N
 
 if __name__ == '__main__':
     RubberBand().cmdloop()
