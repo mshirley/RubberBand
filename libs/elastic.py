2006078a4e51573df6dcbfcca62f8e2fe4a2627d
Adam Maxwell <catalyst256@gmail.com>
2016-06-15 10:11:13 +0100

    tweaks to search

--- libs/elastic.py
+++ libs/elastic.py
@@ -4,12 +4,25 @@ import ConfigParser
 import elasticsearch
 
 
-def searchelastic(index, line):
+def countelastic(index):
     try:
         conf = ConfigParser.ConfigParser()
         conf.read("rubberband.conf")
         es = elasticsearch.Elasticsearch(conf.get('elastic', 'server'))
-        results = es.search(index=index, q='{0}'.format(line))
+        results = es.search(index=index, q='*')
+        data = results['hits']
+        return data['total']
+    except Exception as e:
+        print str(e)
+
+
+def searchelastic(index, line):
+    try:
+        conf = ConfigParser.ConfigParser()
+        conf.read("rubberband.conf")
+        size = countelastic(index)
+        es = elasticsearch.Elasticsearch(conf.get('elastic', 'server'))
+        results = es.search(index=index, size=size, q='{0}'.format(line))
         data = results['hits']
         return data
     except Exception as e:
